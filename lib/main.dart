545f33ba5ad3ddc927cfe3ba28c07145222bc44d
Nii-Gyan Christian <niigyanchristian@gmail.com>
2022-04-01 05:36:09 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import './login_screen.dart';
+import './signUp_screen.dart';
+
+void main() => runApp(const LoginUI());
+
+class LoginUI extends StatelessWidget {
+  const LoginUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "One Don Flutter App",
+          ),
+        ),
+        body: const LoginScreen(),
+        // body: const SignUpScreen(),
+      ),
+    );
+  }
+}
